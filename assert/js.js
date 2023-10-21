9de3f454e5a4418e48e6318b4cb34ed000061c88
Le Trung <minle0008@gmail.com>
2023-10-21 17:28:52 +0700

    Revert "upload header web"
    This reverts commit 66ad788e368748fcd542e731472504a29ca13049.

--- assert/js.js
+++
@@ -1,8 +0,0 @@
-$('select').change(function(){
-    var text = $(this).find('option:selected').text()
-    var $aux = $('<select/>').append($('<option/>').text(text))
-    $(this).after($aux)
-    $(this).width($aux.width())
-    $aux.remove()
-  }).change()
-  
